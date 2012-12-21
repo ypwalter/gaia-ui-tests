ba103e15890848a9af0e05485806fa1f51064420
Dave Hunt <dave.hunt@gmail.com>
2012-12-21 18:22:43 +0000

    Fixing the test for new alarm default values

--- gaiatest/tests/clock/test_clock_all_items_present_new_alarm.py
+++ gaiatest/tests/clock/test_clock_all_items_present_new_alarm.py
@@ -4,56 +4,42 @@
 
 from gaiatest import GaiaTestCase
 from gaiatest.tests.clock import clock_object
-import time
+
 
 class TestClockTestAllItemsPresentNewAlarm(GaiaTestCase):
-    
+
     def setUp(self):
         GaiaTestCase.setUp(self)
-
-        # unlock the lockscreen if it's locked
-        self.lockscreen.unlock()
-
-        # launch the Clock app
         self.app = self.apps.launch('Clock')
-        
-        
-    def test_all_items_present_new_alarm(self):
 
+    def test_all_items_present_new_alarm(self):
         # Wait for the new alarm screen to load
         self.wait_for_element_displayed(*clock_object._alarm_create_new_locator)
+        self.marionette.find_element(*clock_object._alarm_create_new_locator).click()
 
-        picker_container_tagname = self.marionette.find_element(*clock_object._picker_container).tag_name
-        alarm_name_txt = self.marionette.find_element(*clock_object._alarm_name).text
-        repeat_selector_txt = self.marionette.find_element(*clock_object._repeat_menu).text
-        sound_selector_txt = self.marionette.find_element(*clock_object._sound_menu).text
-        snooze_selector_txt = self.marionette.find_element(*clock_object._snooze_menu).text
-        
-        # Ensure that the picker container exists and is displayed
-        self.assertEquals(picker_container_tagname, 'div',
-            'Container was %s' % picker_container_tagname)
-        self.assertTrue(self.marionette.find_element(*clock_object._picker_container)
-            .is_displayed(), 'Picker container not displayed.')
+        # Ensure the picker is displayed
+        picker = self.marionette.find_element(*clock_object._picker_container)
+        self.assertTrue(picker.is_displayed(), 'Picker container not displayed.')
 
-        # Ensure the alarm name input has the default text Alarm
-        self.assertEquals(alarm_name_txt, 'Alarm',
-            'Alarm name was %s' % alarm_name_txt)
+        # Ensure label has the default placeholder and text
+        label = self.marionette.find_element(*clock_object._alarm_name)
+        self.assertEquals(label.get_attribute('placeholder'), 'Alarm')
+        self.assertEquals(label.text, '')
 
-        # If either Never is not the text or it does not exist the below will fail
-        self.assertEquals(repeat_selector_txt, 'Never',
-            'Actual repeat selector text: %s' % repeat_selector_txt)
+        # Ensure repeat has the default value
+        repeat = self.marionette.find_element(*clock_object._repeat_menu)
+        self.assertEquals(repeat.text, 'Never')
 
-        # If either Classic is not the text or it does not exist the below will fail
-        self.assertEquals(sound_selector_txt, 'Classic',
-            'Actual sound selector text: %s' % sound_selector_txt)
+        # Ensure sound has the default value
+        sound = self.marionette.find_element(*clock_object._sound_menu)
+        self.assertEquals(sound.text, 'Classic Buzz')
 
-        # If either 5 minutes is not the text or it does not exist the below will fail
-        self.assertEquals(snooze_selector_txt, '5 minutes',
-            'Actual snooze selector text: %s' % snooze_selector_txt)
-        
-        
+        # Ensure snooze has the default value
+        snooze = self.marionette.find_element(*clock_object._snooze_menu)
+        self.assertEquals(snooze.text, '5 minutes')
+
     def tearDown(self):
-        
+
         # close the app
         if hasattr(self, 'app'):
             self.apps.kill(self.app)
