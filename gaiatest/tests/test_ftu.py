6923311a27127b7696d522acf88c29b60a9eb40f
Bob Silverberg <bob.silverberg@gmail.com>
2013-01-04 16:59:23 -0500

    Update test_ftu to take the tour as well

--- gaiatest/tests/test_ftu.py
+++ gaiatest/tests/test_ftu.py
@@ -51,6 +51,16 @@ class TestFtu(GaiaTestCase):
     # Section Finish
     _section_finish_locator = ('id', 'finish-screen')
     _skip_tour_button_locator = ('id', 'skip-tutorial-button')
+    _take_tour_button_locator = ('id', 'lets-go-button')
+
+    # Section Tour
+    _step1_header_locator = ('id', 'step1Header')
+    _step2_header_locator = ('id', 'step2Header')
+    _step3_header_locator = ('id', 'step3Header')
+    _step4_header_locator = ('id', 'step4Header')
+    _step5_header_locator = ('id', 'step5Header')
+    _tour_next_button_locator = ('id', 'forwardTutorial')
+    _tour_back_button_locator = ('id', 'backTutorial')
 
     # Section Tutorial Finish
     _section_tutorial_finish_locator = ('id', 'tutorialFinish')
@@ -71,7 +81,65 @@ class TestFtu(GaiaTestCase):
         self.app = self.apps.launch('FTU')
 
 
-    def test_ftu(self):
+    def test_ftu_skip_tour(self):
+        # Run through the first part of the FTU
+        self.complete_ftu_up_to_finish()
+
+        # Skip the tour
+        self.marionette.find_element(*self._skip_tour_button_locator).click()
+
+        # Switch back to top level now that FTU app is gone
+        self.marionette.switch_to_frame()
+
+        self.assertTrue(self.data_layer.get_setting("ril.data.enabled"), "Cell data was not enabled by FTU app")
+        self.assertTrue(self.data_layer.is_wifi_connected(self.testvars['wifi']), "WiFi was not connected via FTU app")
+
+
+    def test_ftu_with_tour(self):
+        # Run through the first part of the FTU
+        self.complete_ftu_up_to_finish()
+
+        # Take the tour
+        self.marionette.find_element(*self._take_tour_button_locator).click()
+
+        # Walk through the tour
+        self.wait_for_element_displayed(*self._step1_header_locator)
+        self.assertEqual(self.marionette.find_element(*self._step1_header_locator).text,
+            "Swipe from right to left to browse your apps.")
+        self.marionette.find_element(*self._tour_next_button_locator).click()
+        self.wait_for_element_displayed(*self._step2_header_locator)
+        self.assertEqual(self.marionette.find_element(*self._step2_header_locator).text,
+            "Swipe from left to right to discover new apps.")
+        self.marionette.find_element(*self._tour_next_button_locator).click()
+        self.wait_for_element_displayed(*self._step3_header_locator)
+        self.assertEqual(self.marionette.find_element(*self._step3_header_locator).text,
+            "Tap and hold on an icon to delete or move it.")
+        self.marionette.find_element(*self._tour_next_button_locator).click()
+        self.wait_for_element_displayed(*self._step4_header_locator)
+        self.assertEqual(self.marionette.find_element(*self._step4_header_locator).text,
+            "Swipe down to access recent notifications, credit information and settings.")
+        self.marionette.find_element(*self._tour_next_button_locator).click()
+        self.wait_for_element_displayed(*self._step5_header_locator)
+        self.assertEqual(self.marionette.find_element(*self._step5_header_locator).text,
+            "Tap and hold the home button to browse and close recent apps.")
+        # Try going back a step
+        self.marionette.find_element(*self._tour_back_button_locator).click()
+        self.wait_for_element_displayed(*self._step4_header_locator)
+        self.marionette.find_element(*self._tour_next_button_locator).click()
+        self.wait_for_element_displayed(*self._step5_header_locator)
+        self.marionette.find_element(*self._tour_next_button_locator).click()
+
+        self.wait_for_element_displayed(*self._section_tutorial_finish_locator)
+        self.marionette.find_element(*self._lets_go_button_locator).click()
+
+        # Switch back to top level now that FTU app is gone
+        self.marionette.switch_to_frame()
+
+        self.assertTrue(self.data_layer.get_setting("ril.data.enabled"), "Cell data was not enabled by FTU app")
+        self.assertTrue(self.data_layer.is_wifi_connected(self.testvars['wifi']), "WiFi was not connected via FTU app")
+
+
+    def complete_ftu_up_to_finish(self):
         # https://moztrap.mozilla.org/manage/case/3876/
         # 3876, 3879
 
@@ -155,15 +223,6 @@ class TestFtu(GaiaTestCase):
         # Click next
         self.marionette.find_element(*self._next_button_locator).click()
         self.wait_for_element_displayed(*self._section_finish_locator)
-
-       # The tour appears to be empty so let's skip it
-        self.marionette.find_element(*self._skip_tour_button_locator).click()
-
-        # Switch back to top level now that FTU app is gone
-        self.marionette.switch_to_frame()
-
-        self.assertTrue(self.data_layer.get_setting("ril.data.enabled"), "Cell data was not enabled by FTU app")
-        self.assertTrue(self.data_layer.is_wifi_connected(self.testvars['wifi']), "WiFi was not connected via FTU app")
 
     def tearDown(self):
 
