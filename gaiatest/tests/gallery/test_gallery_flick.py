6686aa80f2c1ce845a4d1ac415c29e9cb011d2f8
bebef1987 <florin.strugariu@softvision.ro>
2013-02-18 15:47:05 +0200

    code review update

--- gaiatest/tests/gallery/test_gallery_flick.py
+++ gaiatest/tests/gallery/test_gallery_flick.py
@@ -45,87 +45,81 @@ class TestGallery(GaiaTestCase):
         self.marionette.tap(first_gallery_item)
         self.wait_for_element_displayed(*self._current_image_locator)
 
-        prev_src = ''
+        previous_image_source = None
 
         # Check the next flicks
         for i in range(len(gallery_items)):
 
-            cur_img_src = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
-            print 'current image is: %s' %(i+1)
+            current_image_source = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
+            print 'current image is: %s' % (i + 1)
 
-            self.assertIsNotNone(cur_img_src)
-            self.assertNotEqual(cur_img_src, prev_src)
+            self.assertIsNotNone(current_image_source)
+            self.assertNotEqual(current_image_source, previous_image_source)
 
             self.assertTrue(self.is_element_displayed(*self._photos_toolbar_locator))
 
-            prev_src = cur_img_src
+            previous_image_source = current_image_source
 
             if i != len(gallery_items) - 1:
-                self._flick_next_image()
+                self.flick_to_image('left')
 
         # try to flick next image (No image should be available)
-        self._flick_next_image()
+        self.flick_to_image('left')
 
-        cur_img_src = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
+        current_image_source = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
         print 'current image is: 4'
 
-        self.assertIsNotNone(cur_img_src)
-        self.assertEqual(cur_img_src, prev_src)
+        self.assertIsNotNone(current_image_source)
+        self.assertEqual(current_image_source, previous_image_source)
 
         self.assertTrue(self.is_element_displayed(*self._photos_toolbar_locator))
 
-        prev_src = cur_img_src
-        time.sleep(2)
+        previous_image_source = current_image_source
 
         # check the prev flick
         for i in range(len(gallery_items) - 1):
 
-            self._flick_prev_image()
+            self.flick_to_image('right')
 
-            cur_img_src = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
-            print 'current image is: %s' %(len(gallery_items) - i)
+            current_image_source = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
+            print 'current image is: %s' % (len(gallery_items) - i)
 
-            self.assertIsNotNone(cur_img_src)
-            self.assertNotEqual(cur_img_src, prev_src)
+            self.assertIsNotNone(current_image_source)
+            self.assertNotEqual(current_image_source, previous_image_source)
 
             self.assertTrue(self.is_element_displayed(*self._photos_toolbar_locator))
 
-            prev_src = cur_img_src
-
+            previous_image_source = current_image_source
 
         # try to flick prev image (No image should be available)
-        self._flick_prev_image()
+        self.flick_to_image('right')
 
-        cur_img_src = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
+        current_image_source = self.marionette.find_element(*self._current_image_locator).get_attribute('src')
         print 'current image is: 1'
 
-        self.assertIsNotNone(cur_img_src)
-        self.assertEqual(cur_img_src, prev_src)
+        self.assertIsNotNone(current_image_source)
+        self.assertEqual(current_image_source, previous_image_source)
 
         self.assertTrue(self.is_element_displayed(*self._photos_toolbar_locator))
 
-    def _flick_next_image(self):
+    def flick_to_image(self, direction):
 
         current_image = self.marionette.find_element(*self._current_image_locator)
 
-        # Flick to next image
-        self.marionette.flick(current_image,  # target element
-                              '50%', '50%',  # start from middle of the target element
-                              '-50%', 0,  # move 50% of width to the left
-                              800)  # gesture duration
+        if direction == 'left':
+            # Flick to next image
+            self.marionette.flick(current_image,  # target element
+                                  '50%', '50%',  # start from middle of the target element
+                                  '-50%', 0,  # move 50% of width to the left
+                                  800)  # gesture duration
+        elif direction == 'right':
+            # Flick to previous image
+            self.marionette.flick(current_image,  # target element
+                                  '50%', '50%',  # start from middle of the target element
+                                  '+50%', 0,  # move 50% of width to the right
+                                  800)  # gesture duration
+        else:
+            self.fail('Provided argument is invalid.\n Expected: "left" or "right"\n Actual: "%s"' % direction)
 
         # Wait for next image to be available
-        self.wait_for_element_displayed(*self._current_image_locator)
-
-    def _flick_prev_image(self):
-
-        current_image = self.marionette.find_element(*self._current_image_locator)
-
-        # Flick to previous image
-        self.marionette.flick(current_image,  # target element
-                              '50%', '50%', # start from middle of the target element
-                              '+50%', 0,  # move 50% of width to the right
-                              800)  # gesture duration
-
-        # Wait for prev image to be available
         self.wait_for_element_displayed(*self._current_image_locator)
