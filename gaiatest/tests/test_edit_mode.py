b8b30ce4bac6b8bb8ac24e754c97feb1e4c7586a
AlinT <trifandreialin@gmail.com>
2013-02-26 11:51:20 +0200

    Test for issue #20
    [Homescreen] Write test to access and exit edit mode

---
+++ gaiatest/tests/test_edit_mode.py
@@ -0,0 +1,40 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest import GaiaTestCase
+
+
+class TestEditMode(GaiaTestCase):
+
+    _delete_app_locator = ('css selector', 'span.options')
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+
+        self.homescreen = self.apps.launch('Homescreen')
+
+    def test_access_and_leave_edit_mode(self):
+
+        self._go_to_next_page()
+
+        # go to edit mode.
+        # TODO: activate edit mode using HOME button https://bugzilla.mozilla.org/show_bug.cgi?id=814425
+        self._activate_edit_mode()
+
+        #verify that the delete app icons appear
+        delete_app_icon = self.marionette.find_element(*self._delete_app_locator)
+        self.assertTrue(delete_app_icon.is_displayed())
+
+        #tap home button and verify that delete app icons are no longor visible
+        self.marionette.switch_to_frame()
+        self._touch_home_button()
+
+    def _touch_home_button(self):
+        self.marionette.execute_script("window.wrappedJSObject.dispatchEvent(new Event('home'));")
+
+    def _go_to_next_page(self):
+        self.marionette.execute_script('window.wrappedJSObject.GridManager.goToNextPage()')
+
+    def _activate_edit_mode(self):
+        self.marionette.execute_script("window.wrappedJSObject.Homescreen.setMode('edit')")
