0f457cf936f9bbfc83b3c5e6b57fa701e61549ed
bebef1987 <florin.strugariu@softvision.ro>
2013-03-01 14:43:35 +0200

    fix issue #428

--- gaiatest/tests/cards_view/test_cards_view_with_three_apps.py
+++ gaiatest/tests/cards_view/test_cards_view_with_three_apps.py
@@ -18,8 +18,6 @@ class TestCardsViewThreeApps(GaiaTestCase):
     _app_card_locator = ('css selector', '#cards-view li.card[data-origin*="%s"]')
     _close_button_locator = ('css selector', '#cards-view li.card[data-origin*="%s"] .close-card')
 
-    _homescreen_frame_locator = ('id', 'lockscreen')
-
     def setUp(self):
         GaiaTestCase.setUp(self)
 
@@ -66,41 +64,6 @@ class TestCardsViewThreeApps(GaiaTestCase):
 
         self.assertFalse(card_view_element.is_displayed(),
                          "Card view not expected to be visible")
-
-    def test_kill_app_from_cards_view(self):
-        # https://moztrap.mozilla.org/manage/case/1917/
-        # go to the home screen
-        self.marionette.switch_to_frame()
-        self._touch_home_button()
-
-        # pull up the cards view
-        self._hold_home_button()
-        self.wait_for_element_displayed(*self._cards_view_locator)
-
-        # Find the close icon for the current app
-        close_third_app_button = self.marionette.find_element(*self.test_apps[2]['close_button_locator'])
-        self.marionette.tap(close_third_app_button)
-
-        self.marionette.switch_to_frame()
-
-        # pull up the cards view again
-        self._hold_home_button()
-        self.wait_for_element_displayed(*self._cards_view_locator)
-
-        # If we successfully killed the app, we should no longer find the app
-        # card inside cards view.
-        self.assertRaises(NoSuchElementException,
-                        self.marionette.find_element,
-                        *self.test_apps[2]['card_locator'])
-
-        # Check if the remaining 2 apps are visible in the cards view
-        self.assertTrue(
-            self.marionette.find_element(*self.test_apps[0]['card_locator']). is_displayed(),
-                "First opened app should not be visible in cards view")
-
-        self.assertTrue(
-            self.marionette.find_element(*self.test_apps[1]['card_locator']). is_displayed(),
-                "Second app opened should be visible in cards view")
 
     def _hold_home_button(self):
         self.marionette.execute_script("window.wrappedJSObject.dispatchEvent(new Event('holdhome'));")
