6969796ca348921fdcad1537a69640315f33d922
Aaron Train <aaron.train@gmail.com>
2013-02-28 21:04:43 -0500

    Assertion/send_keys cleanup in #425 - more cleanup

--- gaiatest/tests/clock/test_clock_create_new_alarm.py
+++ gaiatest/tests/clock/test_clock_create_new_alarm.py
@@ -68,7 +68,8 @@ class TestClockCreateNewAlarm(GaiaTestCase):
         alarm_label_text = "test4321"
 
         alarm_label = self.marionette.find_element(*clock_object._new_alarm_label)
-        alarm_label.send_keys("\b\b\b\b\b%s" % alarm_label_text)
+        alarm_label.clear()
+        alarm_label.send_keys(alarm_label_text)
 
         # save the alarm
         alarm_save = self.marionette.find_element(*clock_object._alarm_save_locator)
@@ -77,7 +78,7 @@ class TestClockCreateNewAlarm(GaiaTestCase):
         # verify the label of alarm
         self.wait_for_element_displayed(*clock_object._alarm_label)
         alarm_label = self.marionette.find_element(*clock_object._alarm_label).text
-        self.assertTrue(alarm_label_text == alarm_label, 'Actual label was: "' + alarm_label + '", not "test4321".')
+        self.assertEqual(alarm_label, alarm_label_text)
 
     def tearDown(self):
         # delete any existing alarms
