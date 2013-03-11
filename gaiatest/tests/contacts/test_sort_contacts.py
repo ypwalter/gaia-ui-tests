c04e337fc040e414ee5094d0c5d72c985d86a0b9
askeing <askeing@gmail.com>
2013-03-11 12:33:57 +0800

    Using the contacts app object.

--- gaiatest/tests/contacts/test_sort_contacts.py
+++ gaiatest/tests/contacts/test_sort_contacts.py
@@ -4,17 +4,10 @@
 
 from gaiatest import GaiaTestCase
 from gaiatest.mocks.mock_contact import MockContact
+from gaiatest.apps.contacts.app import Contacts
 
 
 class TestContacts(GaiaTestCase):
-
-    _loading_overlay = ('id', 'loading-overlay')
-
-    _settings_button_locator = ('id', 'settings-button')
-    _settings_close_button_locator = ('id', 'settings-close')
-    _order_by_last_name_locator = ('css selector', 'p[data-l10n-id="contactsOrderBy"]')
-    _order_by_last_name_switch_locator = ('css selector', 'input[name="order.lastname"]')
-    _contacts_items_locator = ('css selector', '.contact-item p[data-order]')
 
     # contacts name list
     _contacts_name_list = [('GG', 'E'), ('AA', 'Z'), ('XX', 'C'), ('CC', 'X'), ('EE', 'G'), ('FF', 'F'), ('HH', 'D'), ('BB', 'Y'), ('YY', 'B'), ('ZZ', 'A'), ('DD', 'H')]
@@ -30,66 +23,44 @@ class TestContacts(GaiaTestCase):
         self.sorted_contacts_name_by_first = sorted(self._contacts_name_list, key=lambda name: name[0])
         self.sorted_contacts_name_by_last = sorted(self._contacts_name_list, key=lambda name: name[1])
 
-        # launch the Contacts app
-        self.app = self.apps.launch('Contacts')
-        self.wait_for_element_not_displayed(*self._loading_overlay)
-
-    def create_contact_locator(self, contact):
-        return ('css selector', '.contact-item p[data-order^=%s]' % contact)
-
-    def test_add_photo_from_gallery_to_contact(self):
+    def test_sort_contacts(self):
         """ Test sorting of contacts
 
         https://github.com/mozilla/gaia-ui-tests/issues/467
 
         """
 
-        # Make sure the "order-by-last-name" is off before running test
-        self.wait_for_element_displayed(*self._settings_button_locator)
-        # navigate to settings page
-        contact_settings_button = self.marionette.find_element(*self._settings_button_locator)
-        self.marionette.tap(contact_settings_button)
-        self.wait_for_element_displayed(*self._order_by_last_name_locator)
-        order_by_last_name = self.marionette.find_element(*self._order_by_last_name_locator)
-        order_by_last_name_switch = self.marionette.find_element(*self._order_by_last_name_switch_locator)
-        if order_by_last_name_switch.is_selected():
-            # if "Order by last name" switch is on, turn off it
-            self.marionette.tap(order_by_last_name)
-        # go to contacts main page from settings page
-        settings_close_button = self.marionette.find_element(*self._settings_close_button_locator)
-        self.marionette.tap(settings_close_button)
+        contacts_app = Contacts(self.marionette)
+        contacts_app.launch()
+
+        # if "Order by last name" switch is on, turn off it
+        contacts_settings = contacts_app.tap_settings()
+        if contacts_settings.order_by_last_name:
+            contacts_settings.tap_order_by_last_name()
+        contacts_settings.tap_done()
 
         # sort by first name, compare with sorted-by-first-name list
-        self.wait_for_element_displayed(*self._settings_button_locator)
-        contact_items = self.marionette.find_elements(*self._contacts_items_locator)
+        contact_items = contacts_app.contacts
         for idx in range(len(self._contacts_name_list)):
             name_tuple = self.sorted_contacts_name_by_first[idx]
-            self.assertEqual(contact_items[idx].text, name_tuple[0] + " " + name_tuple[1], "Should order by first name.")
+            self.assertEqual(contact_items[idx].full_name, name_tuple[0] + " " + name_tuple[1], "Should order by first name.")
 
         # navigate to settings page
-        contact_settings_button = self.marionette.find_element(*self._settings_button_locator)
-        self.marionette.tap(contact_settings_button)
+        contacts_settings = contacts_app.tap_settings()
         # turn on "Order by last name" switch
-        self.wait_for_element_displayed(*self._order_by_last_name_locator)
-        order_by_last_name = self.marionette.find_element(*self._order_by_last_name_locator)
-        self.marionette.tap(order_by_last_name)
-
+        contacts_settings.tap_order_by_last_name()
         # go to contacts main page from settings page
-        self.wait_for_element_displayed(*self._settings_close_button_locator)
-        settings_close_button = self.marionette.find_element(*self._settings_close_button_locator)
-        self.marionette.tap(settings_close_button)
+        contacts_settings.tap_done()
 
         # sort by last name, compare with sorted-by-last-name list
-        self.wait_for_element_displayed(*self._settings_button_locator)
-        contact_items = self.marionette.find_elements(*self._contacts_items_locator)
+        contact_items = contacts_app.contacts
         for idx in range(len(self._contacts_name_list)):
             name_tuple = self.sorted_contacts_name_by_last[idx]
-            self.assertEqual(contact_items[idx].text, name_tuple[0] + " " + name_tuple[1], "Should order by last name.")
+            self.assertEqual(contact_items[idx].full_name, name_tuple[0] + " " + name_tuple[1], "Should order by last name.")
 
         # navigate to settings page
-        contact_settings_button = self.marionette.find_element(*self._settings_button_locator)
-        self.marionette.tap(contact_settings_button)
+        contacts_settings = contacts_app.tap_settings()
         # turn off "Order by last name" switch
-        self.wait_for_element_displayed(*self._order_by_last_name_locator)
-        order_by_last_name = self.marionette.find_element(*self._order_by_last_name_locator)
-        self.marionette.tap(order_by_last_name)
+        contacts_settings.tap_order_by_last_name()
+        # go to contacts main page from settings page
+        contacts_settings.tap_done()
