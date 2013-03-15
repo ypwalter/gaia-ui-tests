b94358cdf368a66cbfa0311febb49213c3ea31b2
Teodosia <teodosiapop@P4397.(none)>
2013-03-15 17:38:10 +0200

    Test that checks adding a new tab to a browser.

---
+++ gaiatest/tests/browser/test_browser_tabs.py
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest import GaiaTestCase
+from gaiatest.apps.browser.app import Browser
+
+
+class TestBrowserLAN(GaiaTestCase):
+
+    _page_title_locator = ("id", "page-title")
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+
+        if self.wifi:
+            self.data_layer.enable_wifi()
+            self.data_layer.connect_to_wifi(self.testvars['wifi'])
+
+    def tearDown(self):
+        if self.wifi:
+            self.data_layer.disable_wifi()
+        GaiaTestCase.tearDown(self)
