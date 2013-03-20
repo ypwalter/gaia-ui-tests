a2e24600e8d700239a2ded3bde1e43c5f4ebc665
zacc <zcampbell@mozilla.com>
2013-03-20 16:15:03 +0000

    Move lockscreen tests into a folder

--- gaiatest/tests/lockscreen/test_lockscreen_unlock_to_homescreen.py
+++ gaiatest/tests/lockscreen/test_lockscreen_unlock_to_homescreen.py
@@ -12,15 +12,10 @@ class TestLockScreen(GaiaTestCase):
     _lockscreen_area_locator = ('id', 'lockscreen-area')
     _lockscreen_handle_locator = ('id', 'lockscreen-area-handle')
     _unlock_button_locator = ('id', 'lockscreen-area-unlock')
-    _camera_button_locator = ('id', 'lockscreen-area-camera')
 
     # Homescreen locators
     _homescreen_frame_locator = ('css selector', 'div.homescreen iframe')
     _homescreen_landing_locator = ('id', 'landing-page')
-
-    # Camera locators
-    _camera_frame_locator = ('css selector', 'iframe[src^="app://camera"][src$="index.html"]')
-    _capture_button_locator = ('id', 'capture-button')
 
     def setUp(self):
         GaiaTestCase.setUp(self)
@@ -48,26 +43,6 @@ class TestLockScreen(GaiaTestCase):
         landing_element = self.marionette.find_element(*self._homescreen_landing_locator)
 
         self.assertTrue(landing_element.is_displayed(), "Landing element not displayed after unlocking")
-
-    def test_unlock_swipe_to_camera(self):
-        # https://moztrap.mozilla.org/manage/case/2460/
-        self._swipe_and_unlock()
-
-        camera_button = self.marionette.find_element(*self._camera_button_locator)
-        camera_button.click()
-
-        lockscreen_element = self.marionette.find_element(*self._lockscreen_locator)
-        self.wait_for_condition(lambda m: not lockscreen_element.is_displayed())
-
-        self.assertFalse(lockscreen_element.is_displayed(), "Lockscreen still visible after unlock")
-
-        camera_frame = self.marionette.find_element(*self._camera_frame_locator)
-
-        # TODO I would prefer to check visibility of the the frame at this point but bug 813583
-        self.marionette.switch_to_frame(camera_frame)
-
-        # Wait fot the capture button displayed. no need to take a photo.
-        self.wait_for_element_displayed(*self._capture_button_locator)
 
     def _swipe_and_unlock(self):
 
