5ed5bd393964611692c5d9901846f42fddd4bc8f
Walter C <wachen@mozilla.com>
2013-03-21 15:34:57 +0800

    Minor modification

--- gaiatest/tests/cost_control/test_cost_control_ftu.py
+++ gaiatest/tests/cost_control/test_cost_control_ftu.py
@@ -9,22 +9,22 @@ class TestCostControlFTU(GaiaTestCase):
 
     # 1st step screen locators
     _welcome_title_locator = ('css selector', 'h1[data-l10n-id="fte-welcome-title"]')
-    _next_button_locator_1 = ('css selector', 'section#step-1 span[data-l10n-id="next"]')
+    _next_button_locator_1 = ('css selector', '#step-1 span[data-l10n-id="next"]')
 
     # 2nd step screen locators
     _data_report_title_locator = ('css selector', 'h1[data-l10n-id="fte-onlydata2-title"]')
-    _rest_report_period_select_locator = ('css selector', 'section#non-vivo-step-1 select')
-    _next_button_locator_2 = ('css selector', 'section#non-vivo-step-1 span[data-l10n-id="next"]')
+    _rest_report_period_select_locator = ('css selector', '#non-vivo-step-1 select')
+    _next_button_locator_2 = ('css selector', '#non-vivo-step-1 span[data-l10n-id="next"]')
 
     # 3rd step screen locators
-    _data_alert_title_locator = ('css selector', 'section#non-vivo-step-2 h1[data-l10n-id="fte-onlydata3-title"]')
-    _ftu_usage_locator = ('css selector', 'section#non-vivo-step-2 span.tag')
-    _ftu_data_alert_switch_locator = ('css selector', 'section#non-vivo-step-2 label.end input')
-    _ftu_data_alert_label_locator = ('css selector', 'section#non-vivo-step-2 label.end')
-    _capacity_button_locator = ('css selector', 'section#data-limit-dialog form button')
-    _size_input_locator = ('css selector', 'section#data-limit-dialog form input')
-    _usage_done_button_locator = ('css selector', '#data-usage-done-button')
-    _go_button_locator = ('css selector', 'section#non-vivo-step-2 button.recommend')
+    _data_alert_title_locator = ('css selector', '#non-vivo-step-2 h1[data-l10n-id="fte-onlydata3-title"]')
+    _ftu_usage_locator = ('css selector', '#non-vivo-step-2 span.tag')
+    _ftu_data_alert_switch_locator = ('css selector', '#non-vivo-step-2 label.end input')
+    _ftu_data_alert_label_locator = ('css selector', '#non-vivo-step-2 label.end')
+    _capacity_button_locator = ('css selector', '#data-limit-dialog form button')
+    _size_input_locator = ('css selector', '#data-limit-dialog form input')
+    _usage_done_button_locator = ('id', 'data-usage-done-button')
+    _go_button_locator = ('css selector', '#non-vivo-step-2 button.recommend')
 
     _usage_app_title_locator = ('css selector', 'h1[data-l10n-id="usage"]')
 
@@ -71,9 +71,15 @@ class TestCostControlFTU(GaiaTestCase):
         self.wait_for_element_displayed(*self._ftu_usage_locator)
         usage = self.marionette.find_element(*self._ftu_usage_locator)
         self.marionette.tap(usage)
+        # try to get what's the capacity now
         capacity = self.marionette.find_element(*self._capacity_button_locator)
+        # there are two choice in this switch u'GB' or u'MB'. if it is u'GB', try to switch to u'MB'
         if capacity.text == u'GB':
             self.marionette.tap(capacity)
+        # make sure it is u'MB'
+        self.assertEqual(capacity.text, u'MB')
+
+        # clear the original assigned value and set it to 0.1
         size = self.marionette.find_element(*self._size_input_locator)
         size.clear()
         size.send_keys('0.1')
@@ -88,16 +94,16 @@ class TestCostControlFTU(GaiaTestCase):
 
     # temporarily solution for select options
     def _select(self, match_string):
-        # Cheeky Select wrapper until Marionette has its own
-        # Due to the way B2G wraps the app's select box we match on text
+        # cheeky Select wrapper until Marionette has its own
+        # due to the way B2G wraps the app's select box we match on text
 
-        # Have to go back to top level to get the B2G select box wrapper
+        # have to go back to top level to get the B2G select box wrapper
         self.marionette.switch_to_frame()
 
         options = self.marionette.find_elements('css selector', '#value-selector-container li')
         close_button = self.marionette.find_element('css selector', 'button.value-option-confirm')
 
-        # Loop options until we find the match
+        # loop options until we find the match
         for li in options:
             if li.text == match_string:
                 li.click()
@@ -105,5 +111,5 @@ class TestCostControlFTU(GaiaTestCase):
 
         close_button.click()
 
-        # Now back to app
+        # now back to app
         self.marionette.switch_to_frame(self.app.frame)
