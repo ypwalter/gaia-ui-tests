53e9289125deaef149206615b0b345d6f640eb26
pcheng <pcheng@mozilla.com>
2013-03-22 17:47:30 +0800

    #473 Gallery Edit Photo Effect
    I stucked at verifying effect is applied and photo is changed. It seems
    that this part is using canvas to chaging the effects.
    Anyone got some idea about this? Attached is the code.

---
+++ gaiatest/tests/gallery/test_gallery_edit_photo.py
@@ -0,0 +1,75 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import time
+
+from gaiatest import GaiaTestCase
+
+
+class TestGalleryEditPhoto(GaiaTestCase):
+
+    _gallery_items_locator = ('css selector', 'li.thumbnail')
+    _current_image_locator = ('css selector', '#frame2 > img')
+    _photos_toolbar_locator = ('id', 'fullscreen-toolbar')
+
+    _edit_photo_locator = ('id', 'fullscreen-edit-button')
+    _edit_effect_button_locator = ('id', 'edit-effect-button')
+    _effect_options_locator = ('css selector', '#edit-effect-options a')
+    _edit_save_locator = ('id', 'edit-save-button')
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+
+        # add photo to storage
+        self.push_resource('IMG_0001.jpg', destination='DCIM/100MZLLA')
+
+        # launch the Gallery app
+        self.app = self.apps.launch('Gallery')
+
+    def test_gallery_edit_photo(self):
+
+        self.wait_for_element_displayed(*self._gallery_items_locator)
+
+        gallery_items = self.marionette.find_elements(*self._gallery_items_locator)
+        old_count = len(gallery_items)
+        self.assertTrue(old_count > 0)
+
+        self.marionette.tap(gallery_items[0])
+        self.wait_for_element_displayed(*self._current_image_locator)
+
+        current_image = self.marionette.find_element(*self._current_image_locator)
+        photos_toolbar = self.marionette.find_element(*self._photos_toolbar_locator)
+
+        self.assertIsNotNone(current_image.get_attribute('src'))
+        self.assertTrue(photos_toolbar.is_displayed())
+
+        # Tap on Edit button
+        self.wait_for_element_displayed(*self._edit_photo_locator)
+        edit_photo_button = self.marionette.find_element(*self._edit_photo_locator)
+        self.marionette.tap(edit_photo_button)
+
+        # Tap on Effects button
+        self.wait_for_element_displayed(*self._edit_effect_button_locator)
+        edit_effect_button = self.marionette.find_element(*self._edit_effect_button_locator)
+        self.marionette.tap(edit_effect_button)
+
+        # Change effects
+        self.wait_for_element_displayed(*self._effect_options_locator)
+        effects = self.marionette.find_elements(*self._effect_options_locator)
+
+        previous_image_source = None
+        for e in effects:
+            self.marionette.tap(e)
+            time.sleep(2)
+
+            # TBD. Verify the photo is changed.
+
+        edit_save_button = self.marionette.find_element(*self._edit_save_locator)
+        self.marionette.tap(edit_save_button)
+
+        # Verify new Photo is created
+        self.wait_for_element_displayed(*self._gallery_items_locator)
+        # need extra wait until new image is saved and appear on screen
+        time.sleep(3)
+        new_count = len(self.marionette.find_elements(*self._gallery_items_locator))
+        self.assertEqual(old_count + 1, new_count)
