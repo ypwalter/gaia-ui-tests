e572fedb58a8056b540266a212d48643ac253ad9
Dave Hunt <dave.hunt@gmail.com>
2013-04-05 00:20:38 +0100

    Remove import of recently removed clock_object.

--- gaiatest/tests/clock/test_clock_delete_alarm.py
+++ gaiatest/tests/clock/test_clock_delete_alarm.py
@@ -1,8 +1,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from gaiatest import GaiaTestCase
-from gaiatest.tests.clock import clock_object
 from gaiatest.apps.clock.app import Clock
 
 
