031f071e23182733f17047b353c91ff50af43839
bebef1987 <florin.strugariu@softvision.ro>
2013-04-10 21:09:19 +0300

    test_setup_basic_gmail

---
+++ gaiatest/apps/email/app.py
@@ -0,0 +1,131 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest.apps.base import Base
+from gaiatest.apps.base import PageRegion
+from gaiatest.apps.email.regions.setup import SetupEmail
+from gaiatest.apps.email.regions.setup import SetupProgress
+from gaiatest.apps.email.regions.setup import MailSetupDone
+from gaiatest.apps.email.regions.settings import Settings
+from gaiatest.apps.email.regions.settings import EmailAccountSettings
+from gaiatest.apps.email.regions.settings import DeleteConfirmation
+
+
+class Email(Base):
+
+    name = 'E-Mail'
+
+    _header_area_locator = ('css selector', '.msg-list-header.msg-nonsearch-only')
+    _email_locator = ('css selector', '.msg-header-item')
+    _syncing_locator = ('css selector', '.msg-messages-syncing > .small')
+
+    def basic_setup_email(self, name, email, password):
+
+        setup = SetupEmail(self.marionette)
+        setup.type_name(name)
+        setup.type_email(email)
+        setup.type_password(password)
+        setup.tap_next()
+
+        setup_progress = SetupProgress(self.marionette)
+        setup_progress.wait_for_progress_screen_visible()
+        setup_progress.wait_for_progress_screen_not_visible()
+
+        mail_setup_done = MailSetupDone(self.marionette)
+        mail_setup_done.tap_continue()
+        self.wait_for_condition(lambda m: self.is_element_displayed(*self._header_area_locator))
+
+    def delete_email_account(self):
+
+        self.header.tap_menu()
+        self.wait_for_condition(lambda m: self.toolbar.is_settings_visible)
+        self.toolbar.tap_settings()
+        settings = Settings(self.marionette)
+        settings.email_accounts[0].tap()
+        account_settings = EmailAccountSettings(self.marionette)
+        account_settings.tap_delete()
+        delete_confirmation = DeleteConfirmation(self.marionette)
+        delete_confirmation.tap_delete()
+
+    @property
+    def header(self):
+        return Header(self.marionette)
+
+    @property
+    def toolbar(self):
+        return ToolBar(self.marionette)
+
+    @property
+    def mails(self):
+        return [Message(self.marionette, mail) for mail in self.marionette.find_elements(*self._email_locator)]
+
+    def wait_for_emails_to_sync(self):
+        self.wait_for_element_not_displayed(*self._syncing_locator)
+
+
+class Header(Base):
+    _menu_button_locator = ('css selector', '.msg-folder-list-btn')
+    _compose_button_locator = ('css selector', '.msg-compose-btn')
+    _label_locator = ('css selector', '.msg-list-header-folder-label.header-label')
+
+    def tap_menu(self):
+        self.marionette.tap(self.marionette.find_element(*self._menu_button_locator))
+
+    def tap_compose(self):
+        self.marionette.tap(self.marionette.find_element(*self._compose_button_locator))
+
+    @property
+    def label(self):
+        return self.marionette.find_element(*self._label_locator).text
+
+    @property
+    def is_menu_visible(self):
+        return self.is_element_displayed(*self._menu_button_locator)
+
+    @property
+    def is_compose_visible(self):
+        return self.is_element_displayed(*self._compose_button_locator)
+
+
+class ToolBar(Base):
+    _refresh_locator = ('css selector', '.msg-refresh-btn')
+    _search_locator = ('css selector', '.msg-search-btn')
+    _edit_locator = ('css selector', '.msg-edit-btn')
+    _settings_locator = ('css selector', '.fld-nav-settings-btn')
+
+    def tap_refresh(self):
+        self.marionette.tap(self.marionette.find_element(*self._refresh_locator))
+
+    def tap_search(self):
+        self.marionette.tap(self.marionette.find_element(*self._search_locator))
+
+    def tap_edit(self):
+        self.marionette.tap(self.marionette.find_element(*self._edit_locator))
+
+    def tap_settings(self):
+        self.marionette.tap(self.marionette.find_element(*self._settings_locator))
+
+    @property
+    def is_refresh_visible(self):
+        return self.is_element_displayed(*self._refresh_locator)
+
+    @property
+    def is_search_visible(self):
+        return self.is_element_displayed(*self._search_locator)
+
+    @property
+    def is_edit_visible(self):
+        return self.is_element_displayed(*self._edit_locator)
+
+    @property
+    def is_settings_visible(self):
+        return self.is_element_displayed(*self._settings_locator)
+
+
+class Message(PageRegion):
+    _subject_locator = ('css selector', '.msg-header-subject')
+
+    @property
+    def subject(self):
+        return self.root_element.find_element(*self._subject_locator).text
---
+++ gaiatest/apps/email/regions/setup.py
@@ -0,0 +1,42 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest.apps.base import Base
+
+
+class SetupEmail(Base):
+
+    _name_locator = ('css selector', 'input.sup-info-name')
+    _email_locator = ('css selector', 'input.sup-info-email')
+    _password_locator = ('css selector', 'input.sup-info-password')
+    _next_locator = ('css selector', '.sup-info-next-btn')
+
+    def type_name(self, value):
+        self.marionette.find_element(*self._name_locator).send_keys(value)
+
+    def type_email(self, value):
+        self.marionette.find_element(*self._email_locator).send_keys(value)
+
+    def type_password(self, value):
+        self.marionette.find_element(*self._password_locator).send_keys(value)
+
+    def tap_next(self):
+        self.marionette.tap(self.marionette.find_element(*self._next_locator))
+
+
+class SetupProgress(Base):
+    _progress_locator = ('css selector', '.sup-progress-label')
+
+    def wait_for_progress_screen_visible(self):
+        self.wait_for_element_displayed(*self._progress_locator)
+
+    def wait_for_progress_screen_not_visible(self):
+        self.wait_for_element_not_displayed(*self._progress_locator)
+
+
+class MailSetupDone(Base):
+    _continue_button_locator = ('class name', 'sup-show-mail-btn sup-form-btn recommend')
+
+    def tap_continue(self):
+        self.marionette.tap(self.marionette.find_element(*self._continue_button_locator))
