adc3a56645459e92e150169a99e61b95a935912a
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2013-05-10 17:43:26 +0300

    fix media storage

--- gaiatest/tests/settings/test_settings_media_storage.py
+++ gaiatest/tests/settings/test_settings_media_storage.py
@@ -11,9 +11,9 @@ class TestSettingsMediaStorage(GaiaTestCase):
     _media_storage_locator = ('id', 'menuItem-mediaStorage')
 
     # Media storage locators
-    _music_space_locator = ('css selector', '#music-space > a > .size')
-    _pictures_space_locator = ('css selector', '#pictures-space > a > .size')
-    _movies_space_locator = ('css selector', '#videos-space > a > .size')
+    _music_space_locator = ('css selector', '.color-music > a > .size')
+    _pictures_space_locator = ('css selector', '.color-pictures > a > .size')
+    _movies_space_locator = ('css selector', '.color-videos > a > .size')
 
     def test_settings_media_storage(self):
 
@@ -61,7 +61,6 @@ class TestSettingsMediaStorage(GaiaTestCase):
         # Tap on 'Media storage'
         self.marionette.execute_script("arguments[0].scrollIntoView(false);", [media_storage_item])
         self.marionette.tap(media_storage_item)
-
         self.wait_for_element_displayed(*self._music_space_locator)
         self.wait_for_element_displayed(*self._pictures_space_locator)
         self.wait_for_element_displayed(*self._movies_space_locator)
