c47c6d14c53f7943cc8326a9feb8db2aeaab318d
zacc <zcampbell@mozilla.com>
2013-05-20 16:03:35 +0100

    [v1-train] Trigger video player controls via javascript to be reliable Compare the time of the video playing by time rather than string

--- gaiatest/apps/videoplayer/regions/fullscreen_video.py
+++ gaiatest/apps/videoplayer/regions/fullscreen_video.py
@@ -2,8 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import time
 from gaiatest.apps.base import Base
-from marionette.errors import ElementNotVisibleException
 
 
 class FullscreenVideo(Base):
@@ -11,25 +11,23 @@ class FullscreenVideo(Base):
     _video_controls_locator = ('id', 'videoControls')
     _video_title_locator = ('id', 'video-title')
     _elapsed_text_locator = ('id', 'elapsed-text')
-    _video_title_locator = ('id', 'video-title')
     _video_player_locator = ('id', 'player')
     _video_frame_locator = ('css selector', "iframe[src^='app://video'][src$='view.html']")
-    _video_player_frame_locator = ('id', 'videoFrame')
+    _video_player_frame_locator = ('id', 'fullscreen-view')
     _video_loaded_locator = ('css selector', 'video[style]')
 
-    def tap_control_bar(self):
-        self.wait_for_element_displayed(*self._video_controls_locator)
-        video_controls_locator = self.marionette.find_element(*self._video_controls_locator)
-        try:
-            self.marionette.tap(video_controls_locator)
-        except ElementNotVisibleException:
-            self.marionette.find_element(*self._video_player_locator).tap()
+    def wait_for_player_frame_displayed(self):
+        self.wait_for_element_displayed(*self._video_player_frame_locator)
 
-        self.wait_for_element_displayed(*self._elapsed_text_locator)
+    def display_controls_with_js(self):
+        self.marionette.execute_script("window.wrappedJSObject.showVideoControls(true);")
+        self.wait_for_element_displayed(*self._video_controls_locator)
 
     @property
     def elapsed_time(self):
-        return self.marionette.find_element(*self._elapsed_text_locator).text
+        # Convert it to a real time so we can accurately assert
+        text = self.marionette.find_element(*self._elapsed_text_locator).text
+        return time.strptime(text, '%M:%S')
 
     @property
     def name(self):
@@ -41,5 +39,4 @@ class FullscreenVideo(Base):
 
     def switch_to_video_frame(self):
         self.marionette.switch_to_frame(self.marionette.find_element(*self._video_frame_locator))
-        self.wait_for_element_displayed(*self._video_player_frame_locator)
         self.wait_for_element_displayed(*self._video_loaded_locator)
