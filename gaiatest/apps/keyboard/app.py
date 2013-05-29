9d23fed091f34f3e949df182dbb93de7eb01a83e
zacc <zcampbell@mozilla.com>
2013-05-29 13:36:19 +0100

    Migrate keyboard, test_notification_bar to use el.tap()
    Conflicts:
    
    	gaiatest/apps/keyboard/app.py

--- gaiatest/apps/keyboard/app.py
+++ gaiatest/apps/keyboard/app.py
@@ -245,8 +245,8 @@ class Keyboard(Base):
 
     def tap_backspace(self):
         self._switch_to_keyboard()
-        bs = self.marionette.find_element(self._button_locator[0], self._button_locator[1] % self._backspace_key)
-        self.marionette.tap(bs)
+        backspace = self.marionette.find_element(self._button_locator[0], self._button_locator[1] % self._backspace_key)
+        backspace.tap()
         self.marionette.switch_to_frame()
 
     def tap_space(self):
