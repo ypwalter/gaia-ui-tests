0b7ba4b1398bd6c51fd2c6f2763f8d1ac14edd1c
Jonathan Griffin <jgriffin@mozilla.com>
2013-05-31 11:10:32 -0700

    Bump marionette_client version number to pick up fix for bug 878133, a=stephend

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except (OSError, IOError):
     description = ''
 
 # dependencies
-deps = ['marionette_client==0.5.27', 'mozdevice']
+deps = ['marionette_client==0.5.28', 'mozdevice']
 
 # copy atoms directory over
 setupdir = os.path.dirname(__file__)
