2e82ba95b8bb0de4824c63e69070c19207ecb4b1
Walter C <wachen@mozilla.com>
2013-06-19 15:58:13 +0800

    Added comments and minor changes

--- gaiatest/tests/marketplace/test_marketplace_feedback_anonymous.py
+++ gaiatest/tests/marketplace/test_marketplace_feedback_anonymous.py
@@ -10,7 +10,6 @@ class TestMarketplaceFeedback(GaiaTestCase):
     MARKETPLACE_DEV_NAME = 'Marketplace Dev'
     cmp_message = u'Feedback submitted. Thanks!'
 
-    _settings_button_locator = ('css selector', 'a.header-button.settings')
     _feedback_tab_locator = ('css selector', 'a[href="/feedback"]')
     _feedback_textarea_locator = ('name', 'feedback')
     _submit_button_locator = ('css selector', 'button[type="submit"]')
@@ -23,25 +22,31 @@ class TestMarketplaceFeedback(GaiaTestCase):
         self.install_marketplace()
 
     def test_marketplace_feedback_anonymous(self):
+        # launch marketplace dev and go to marketplace
         self.marketplace = Marketplace(self.marionette, self.MARKETPLACE_DEV_NAME)
         self.marketplace.launch()
         self.marionette.switch_to_frame()
         self.marketplace.switch_to_marketplace_frame()
 
-        self.wait_for_element_displayed(*self._settings_button_locator)
-        self.marionette.find_element(*self._settings_button_locator).tap()
+        # wait for settings button to come out
+        self.marketplace.wait_for_setting_displayed()
+        settings = self.marketplace.tap_settings()
         self.marionette.find_element(*self._feedback_tab_locator).tap()
 
+        # enter your feedback
         feedback = self.marionette.find_element(*self._feedback_textarea_locator)
         feedback.clear()
         feedback.send_keys('This is a test comment.')
         self.marionette.execute_script('document.getElementsByName("feedback")[0].blur();')
 
+        # submit your comment after keyboard disappears
         self.wait_for_element_displayed(*self._submit_button_locator)
         self.marionette.find_element(*self._submit_button_locator).tap()
 
+        # catch the notification
         self.wait_for_element_displayed(*self._notification_locator)
         message = self.marionette.find_element(*self._notification_content_locator)
         message_content = message.get_attribute('innerHTML')
 
+       # verify if the notification is right
         self.assertEqual(message_content, self.cmp_message)
